7d8e5eaf74fc5669a9758aeb6ab3257d3790448b
Trinadia K <91521969+trinadia@users.noreply.github.com>
2023-08-16 16:04:33 +0700

    Create config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Wi-Fi credentials
+#define WIFI_SSID "Omah Nadhindra 01"
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+
+// Telegram BOT Token
+#define BOT_TOKEN "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+#endif // CONFIG_H
